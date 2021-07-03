53ba970991fdc42c724360d18adb5869b27a3085
philip howley <howley.phil@gmail.com>
2021-07-03 10:30:10 -0400

    Convert to vanilla js

--- index.js
+++ index.js
@@ -1,58 +1,10 @@
-// Example 1
-// We've seen a few ways to define functions:
-// slightly different but largely function similarly
+const demoEl = document.querySelector('#demo');
+const btnEl = document.querySelector('.date-btn');
+console.log(btnEl);
 
-function foo() {
-  console.log('foo');
+function getTime() {
+  demoEl.textContent = Date();
 }
 
-var bar = function () {
-  console.log('bar');
-};
+btnEl.addEventListener('click', getTime);
 
-const baz = function () {
-  console.log('baz');
-};
-
-function subtractWithOptionalThird(num1, num2, num3) {
-  if (num3) {
-    return num1 - num2 - num3;
-  } else {
-    return num1 - num2;
-  }
-}
-const subtractWithOptionalThird = (num1, num2, num3) => {
-  if (num3) {
-    return num1 - num2 - num3;
-  } else {
-    return num1 - num2;
-  }
-};
-
-const subtractWithOptionalThird = (num1, num2, num3) =>
-  num3 ? num1 - num2 - num3 : num1 - num2;
-// Example 2
-
-// Now we're going to learn a short-hand that has one glaring and very important difference. This is called an arrow function.
-
-// Let's convert this add function:
-
-// Start code
-// const add = function(num1, num2) {
-//   return num1 + num2;
-// };
-
-// const add = num1 => num1;
-
-// First we replace the function keyword with an arrow:
-
-// const add = (num1, num2) => {
-//   return num1 + num2;
-// };
-
-// Example 3 - Refactor even more
-// If our function just has one line of logic that we want to return we can eliminate the return keyword and curly braces!
-// const add = (num1, num2) => num1 + num2;
-
-// Next
-// $ git checkout 08-arrow-func-activity-start
